7f55cb856575b464d586d4d15bf8fb50b54838a5
AntonioMaldonado <antonio.manilla.maldonado@gmail.com>
2022-08-06 15:10:23 -0500

    First Commit
    Addition of footer
    Addition of table (without functionality)
    Addition of logos

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # NASA_MarsRoverPhotos
 SDJS-01 Interview Excercise
 
+[https://api.nasa.gov/] 
+
 [https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './components/img/NASA_logo.png';
+import React from 'react';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt='logo' className="App-logo"></img>
+        <h1>Mars Rover Photos</h1>
       </header>
+
+    <table className="table">
+      <tbody>
+        <tr>
+          <th className=".table-row1">Curiosity Cameras</th>
+          <th className=".table-row2">Pictures</th>
+        </tr>
+
+        <tr>
+          <td><a href= "https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Front Hazard Avoidance Camera</a></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td><a href="https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Rear Hazard Avoidance Camera</a></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td><a href="https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Mast Camera</a></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td><a href="https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Chemistry and Camera Complex</a></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td><a href="https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Mars Hand Lens Imager</a></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td><a href="https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Mars Descent Imager</a></td>
+          <td></td>
+        </tr>
+
+        <tr>
+          <td><a href="https://AntonioMM8506.github.io/NASA_MarsRoverPhotos/">Navigation Camera</a></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+
+    <Footer></Footer>
     </div>
   );
-}
+}//End of App
 
 export default App;
